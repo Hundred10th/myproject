3504a18ac08a13188e73c1b12df2c9aa9503c2e0
Hundred10th <tomascant02001@hotmail.com>
2022-02-24 09:09:48 +0000

    myproject1.01

--- routes/roomsRoutes.js
+++ routes/roomsRoutes.js
@@ -2,9 +2,15 @@ var express = require('express');
 var router = express.Router();
 var rModel = require("../models/roomsModel");
             
-router.get('/', async function(req, res, next) {
-    let result = await rModel.getAllRooms();
-    res.status(result.status).send(result.result);
-});
+// router.get('/', async function(req, res, next) {
+//     let result = await rModel.getAllRooms();
+//     res.status(result.status).send(result.result);
+// });
             
+router.get('/:id', async function(req, res, next) {
+    let id = req.params.id;
+    console.log("Get room with id "+id)
+    let result = await rModel.getRoomById(id);
+    res.status(result.status).send(result.result);
+  });
 module.exports = router;
